e45b2f8609ea83f551d69419edf2f361189573ff
J Hoon <function7th@gmail.com>
2022-02-11 11:05:23 +0900

    음식점 id입력시 객체(후기,평점) 반환 메소드 추가

--- DB_Project/src/ReviewAndRatings/ReviewDB/ReviewRepository.java
+++ DB_Project/src/ReviewAndRatings/ReviewDB/ReviewRepository.java
@@ -1,6 +1,6 @@
 package ReviewAndRatings.ReviewDB;
 
-import java.awt.List;
+import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import ReviewAndRatings.ReviewDB.*;
 import busan.ConnectionProvider;
 
 public class ReviewRepository {
@@ -40,23 +39,55 @@ public class ReviewRepository {
 		}
 	}
 
-//	public void joinBpmReview() {
-//		String join = "SELECT * FROM busan.bpm AS A"
-//				+ "LEFT JOIN busan.review AS B "
-//				+ "ON A.id = B.bpm_id;";
-//		List<ReviewInput> list = new ArrayList<>();
-//		try (Connection conn = ConnectionProvider.getConnection();
-//				Statement stmt = conn.createStatement();
-//				ResultSet rs = stmt.executeQuery(join);) {
-//			while (rs.next()) {
-//				String review = rs.getString("review");
-//				Double rating = rs.getDouble("rating");
-//				List.add(new ReviewInput(review, rating));
-//			}
-//			
-//		}catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
 
+	public void joinBpmReview() {
+		String join = "SELECT * FROM busan.bpm AS A " + "LEFT JOIN busan.review AS B " + "ON A.id = B.bpm_id;";
+//		List<ReviewInput> list = new ArrayList<>();
+		System.out.println(join);
+		try (Connection conn = ConnectionProvider.getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(join);) {
+			while (rs.next()) {
+				String review = rs.getString("review");
+				Double rating = rs.getDouble("rating");
+//				List.add(new ReviewInput(review, rating));
+				System.out.println("review" + "rating");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//ResultSet 입력받으면 ReviewInput객체 반환
+	private ReviewInput returnReview(ResultSet rs) throws SQLException {
+		String review = rs.getString("review");
+		double rating = rs.getDouble("rating");
+		
+		return new ReviewInput(review, rating);
+	}
+	
+	// 음식점의 id를 입력받으면 해당 id에 해당하는 ReviewInput 객체(후기, 평점)를 반환
+	public List<ReviewInput> viewReviewAtBpmId(int id) {
+		List<ReviewInput> list = new ArrayList<>();
+		String view = "SELECT * FROM busan.bpm" 
+				+ " WHERE bpm_id = ?";
+
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(view);) {
+			stmt.setInt(1, id);
+
+			try (ResultSet rs = stmt.executeQuery();) {
+				while (rs.next()) {
+					list.add(returnReview(rs));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
+
+
+
